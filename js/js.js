e006cd46d85b2d2a6950aeb9fd8794bc36eedc0d
arnif <arnifa@gmail.com>
2014-01-19 15:32:16 +0000

    cool

--- js/js.js
+++ js/js.js
@@ -1,19 +1,35 @@
 $(document).ready( function() {
-	 //alert("hasdi");
 
   var canvas, context, tool, tempCanvas, tempContext;
 
   var mousedown = false;
   var mouse = {x:0, y:0}
   var lastMouse = {x:0, y:0}
-  var rect = {};
+  var startX = 0;
+  var startY = 0;
+  
+  var w = 0;
+  var h = 0;
+  var r = 0;
 
+  var tempShape;
   var shapeArr = [];
-  var shape = {
-      lineWidth : 4,
-      lineCap : 'round',
-      strokeStyle : 'black',
-      cord : {x:0, y:0}
+  function shape() {
+
+    this.lineWidth = context.lineWidth;
+    this.lineCap = context.lineCap;
+    this.strokeStyle = context.strokeStyle;
+    this.tool = tool;
+    this.cordX = this.mouse.x;
+    this.cordY = this.mouse.y;
+      // lineWidth : 4,
+      // lineCap : 'round',
+      // strokeStyle : 'black',
+      // tool : 'pencil',
+      // cord : {x:0, y:0},
+      // w: 0,
+      // h: 0,
+      // r: 0
   };
 
   tempCanvas = document.getElementById('myCanvas');
@@ -28,7 +44,6 @@ $(document).ready( function() {
 
   context = canvas.getContext('2d');
 
-
   canvas.addEventListener('mousedown', onmousedown, false);
   canvas.addEventListener('mouseup', onmouseup, false);
   canvas.addEventListener('mousemove', onmousemove, false);
@@ -41,7 +56,11 @@ $(document).ready( function() {
     context.strokeStyle = 'black';
     context.fillStyle = 'black';
     canvas.style.cursor = 'crosshair';
-  
+    $(".selected").removeClass("selected");
+    $("#pencil").addClass("selected");
+    $("#black").addClass("selected");
+    $("#brush-value").html("4");
+
   }
 
   function imgUpdate() {
@@ -51,73 +70,113 @@ $(document).ready( function() {
 
   function onmousedown(ev) {
     mousedown = true;
-    if (tool === 'rect') {
-      rect.startX = ev.pageX - this.offsetLeft;
-      rect.startY = ev.pageY - this.offsetTop;
-    }
-    ev.preventDefault();
+    startX = ev.pageX - this.offsetLeft;
+    startY = ev.pageY - this.offsetTop;
+
+    if (tool === 'text') {
+        $("#text-box").show();
+        $("#text-box").css("top", mouse.y);
+        $("#text-box").css("left", mouse.x);
+        $("#text-box").focus();
+      }
     //bua til nytt shape object
+    tempShape = new shape;
     
   }
 
   function onmouseup(ev) {
     mousedown = false;
+    mouse.finalX = ev.pageX - this.offsetLeft;
+    mouse.finalY = ev.pageY - this.offsetTop;
+    if (tool === 'text') {
+      $("#text-box").focus();
+    }
     imgUpdate();
-    ev.preventDefault();
     //setja shape objectid i array
   }
 
   function onmousemove(ev) {
     lastMouse.x = mouse.x;
     lastMouse.y = mouse.y;
-    mouse.x = ev.pageX - this.offsetLeft;
-    mouse.y = ev.pageY - this.offsetTop;
+    var x = mouse.x = ev.pageX - this.offsetLeft;
+    var y = mouse.y = ev.pageY - this.offsetTop;
 
     if (mousedown) {
 
       if(tool === 'rect') {
-        rect.w = (ev.pageX - this.offsetLeft) - rect.startX;
-        rect.h = (ev.pageY - this.offsetTop) - rect.startY ;
+
+        var w = (ev.pageX - this.offsetLeft) - startX;
+        var h = (ev.pageY - this.offsetTop) - startY ;
         context.clearRect(0,0,canvas.width,canvas.height);
-      } 
-      paint(mouse.x,mouse.y);
+        context.strokeRect(startX, startY, w, h);
+
+      } else if (tool === 'pencil') {
+
+        context.beginPath();
+        context.moveTo(lastMouse.x, lastMouse.y);
+        context.lineTo(x, y);
+        context.stroke();
+        context.closePath();
+
+      } else if (tool === 'circle') {
+
+        var cx = (ev.pageX - this.offsetLeft) - startX;
+        var cy = (ev.pageY - this.offsetTop) - startY;
+        context.clearRect(0,0,canvas.width,canvas.height);
+        context.beginPath();
+        context.arc(startX, startY, Math.abs(cy + cx) ,0,Math.PI*2,true);
+        context.stroke();
+        context.closePath();
+
+      } else if (tool === 'line') {
+
+        context.clearRect(0,0,canvas.width,canvas.height);
+        context.beginPath();
+        context.moveTo(startX, startY);
+        context.lineTo(x, y);
+        context.stroke();
+
+      } else if (tool === 'text') {
+
+        $("#text-box").show();
+        $("#text-box").css("top", mouse.y);
+        $("#text-box").css("left", mouse.x);
+        $("#text-box").focus();
+
+        $("#text-box").keyup(function(e) {
+          
+          if (e.keyCode === 13) {
+            context.font = "14px Helvetica ";
+            var text = $("#text-box").val();
+            context.fillText(text, mouse.finalX, mouse.finalY);
+            imgUpdate();
+            $("#text-box").val("");
+            $("#text-box").hide();
+         
+          } else if (e.keyCode === 27) {
+
+            $("#text-box").val("");
+            $("#text-box").hide();
+          }
+
+        });
+
+      }
 
     }
   }
-
-  function paint(x,y) {
-    //setja x og y gildi i shape objectid (array af x og y)
-
-    if (tool === 'pencil') {
-      context.beginPath();
-      context.moveTo(lastMouse.x, lastMouse.y);
-      context.lineTo(x, y);
-      context.stroke();
-      context.closePath();
-
-    } else if (tool === 'rect') {
-      context.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
-
-      // context.stroke();
-      // context.closePath();
-    } else if (tool === 'circle') {
-      context.clearRect(0,0,canvas.width,canvas.height);
-      context.beginPath();
-      context.arc(x,y,100,0,Math.PI*2,true);
-      context.stroke();
-    }
-  }
-
 
   $("#clearBoard").click( function () {
     clearBoard();
-  })
+  });
 
   function clearBoard() {
-    tempCanvas.width = tempCanvas.width;
-    canvas.width = canvas.width;
-
-    setup();
+    var areYouSure = confirm("Are you sure ?");
+    if (areYouSure) {
+      tempCanvas.width = tempCanvas.width;
+      canvas.width = canvas.width;
+      setup();
+    } 
   }
 
   $("#brush-size").on('change', function() {
@@ -127,6 +186,9 @@ $(document).ready( function() {
   });
 
   $("#eraser").click( function() {
+    $(".color").removeClass("selected");
+    $(".tool").removeClass("selected");
+    $("#eraser").addClass("selected");
     context.strokeStyle = 'white';
     context.lineCap = 'round';
     tool = 'pencil';
@@ -146,27 +208,6 @@ $(document).ready( function() {
     tool = this.id;
   });
 
-  // $("#pencil").click( function() {
-  //   // console.log("pensil");
-  //   context.lineCap = 'round';
-  //   tool = 'pencil';
-  // });
-
-  // $("#rect").click( function() {
-  //   // console.log("rect");
-  //   context.lineCap = 'round';
-  //   tool = 'rect';
-  // });
-
-  // $("#circle").click( function() {
-  //   // console.log("rect");
-  //   context.lineCap = 'round';
-  //   tool = 'circle';
-  // });
-
-
-  
-
 setup();
-	
+
 });
